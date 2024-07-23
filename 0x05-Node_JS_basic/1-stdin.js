2c1bddae076da3123574f73fefc510632eed9c52
Dubemxe <dubemxe@gmail.com>
2024-07-23 20:43:08 +0000

    Task_1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rdl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rdl.question('Welcome to Holberton School, what is your name?\n', (input) => {
+	// print the input to the console
+	console.log(`Your name is: ${input}`);
+
+	rdl.close();
+	
+});
+
+rdl.on('close', () => {
+	console.log('This important software is now closing');
+
+});
